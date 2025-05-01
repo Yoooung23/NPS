7c83adee3ca974fac8f616361c5b938961d87ba9
Yoooung23 <logan7design@gmail.com>
2025-05-01 13:31:17 -0600

    JASON!!!

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+function parkInfoTemplate(info) {
+    return ` <a href="/" class="hero-banner__title">${info.name}<a/>
+    <p class="hero-banner"__subtitle">
+        `
+}
