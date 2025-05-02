329bbeb44a77f5420ed1ae1e42e80cb23d8a7868
Yoooung23 <logan7design@gmail.com>
2025-05-01 23:28:12 -0600

    getting there.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,16 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
 function parkInfoTemplate(info) {
-    return ` <a href="/" class="hero-banner__title">${info.name}<a/>
-    <p class="hero-banner"__subtitle">
-        `
-}
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
+  
+  console.log("complete")
