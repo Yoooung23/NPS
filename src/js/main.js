2c019cfa770ddebd8a41ce76016100c1445bb901
Yoooung23 <logan7design@gmail.com>
2025-05-03 12:42:38 -0600

    recipe HW

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,30 @@ disclaimer.href = parkData.url;
 disclaimer.innerHTML = parkData.fullName;
 
 function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    return `<a href="${info.url}" class="hero-banner__title">${info.name}</a>
     <p class="hero-banner__subtitle">
       <span>${info.designation}</span>
       <span>${info.states}</span>
     </p>`;
-  }
-  
-  console.log("complete")
+}
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-banner img");
+if (heroImage && parkData.images && parkData.images.length > 0) {
+    heroImage.src = parkData.images[0].url;
+    heroImage.alt = parkData.images[0].altText || parkData.fullName;
+}
+
+const heroContent = document.querySelector(".hero-banner__content");
+if (heroContent) {
+    const statesTrimmed = parkData.states ? parkData.states.replace(/,\s*$/, "") : "";
+    heroContent.innerHTML = parkInfoTemplate({
+        url: parkData.url,
+        name: parkData.name,
+        designation: parkData.designation,
+        states: statesTrimmed
+    });
+}
+
+console.log("complete");
